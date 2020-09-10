f5b8417bd84892ad1f93381b515088af44d86d0c
Gareth Rushgrove <gareth@morethanseven.net>
2020-09-10 23:07:10 +0100

    Support installing a specific version

--- install/install_snyk.sh
+++ install/install_snyk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-wget -qO- https://api.github.com/repos/snyk/snyk/releases/latest | grep "browser_download_url" | grep linux | cut -d '"' -f 4 | wget --progress=bar:force:noscroll -i - && \
+wget -qO- https://api.github.com/repos/snyk/snyk/releases/${1} | grep "browser_download_url" | grep linux | cut -d '"' -f 4 | wget --progress=bar:force:noscroll -i - && \
     sha256sum -c snyk-linux.sha256 && \
     chmod +x snyk-linux && \
     sudo mv snyk-linux /usr/local/bin/snyk
