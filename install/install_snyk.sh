a91e18a1c55e5eda973a4c49f5bebfaefd3028ac
Gareth Rushgrove <gareth@morethanseven.net>
2020-09-10 22:49:23 +0100

    Hard code version to test rest of script

--- install/install_snyk.sh
+++ install/install_snyk.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Installing Snyk ($1)"
-
-curl -s https://api.github.com/repos/snyk/snyk/releases/$1 | grep "browser_download_url" | grep linux | cut -d '"' -f 4 | wget -i - && \
+curl -s https://api.github.com/repos/snyk/snyk/releases/latest | grep "browser_download_url" | grep linux | cut -d '"' -f 4 | wget -i - && \
     sha256sum -c snyk-linux.sha256 && \
     mv snyk-linux /usr/local/bin/snyk && \
     chmod +x /usr/local/bin/snyk
