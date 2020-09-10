43a075c3cb0cbde552dc6a4d4a438b7eda4ff147
Gareth Rushgrove <gareth@morethanseven.net>
2020-09-10 23:00:11 +0100

    Try using sudo to install on the path

--- install/install_snyk.sh
+++ install/install_snyk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-wget -qO- https://api.github.com/repos/snyk/snyk/releases/latest | grep "browser_download_url" | grep linux | cut -d '"' -f 4 | wget -i - && \
+wget -qO- https://api.github.com/repos/snyk/snyk/releases/latest | grep "browser_download_url" | grep linux | cut -d '"' -f 4 | wget --progress=bar:force:noscroll -i - && \
     sha256sum -c snyk-linux.sha256 && \
-    mv snyk-linux /usr/local/bin/snyk && \
-    chmod +x /usr/local/bin/snyk
+    chmod +x snyk-linux && \
+    sudo mv snyk-linux /usr/local/bin/snyk
