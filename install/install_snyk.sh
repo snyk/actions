974f15734d58054ab270a0160892c9f8ae92e05d
Gareth Rushgrove <gareth@morethanseven.net>
2020-09-10 22:46:21 +0100

    Explicitally pass the version to the script

--- install/install_snyk.sh
+++ install/install_snyk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "Installing Snyk (${INPUT_VERSION})"
+echo "Installing Snyk ($1)"
 
-curl -s https://api.github.com/repos/snyk/snyk/releases/${INPUT_VERSION} | grep "browser_download_url" | grep linux | cut -d '"' -f 4 | wget -i - && \
+curl -s https://api.github.com/repos/snyk/snyk/releases/$1 | grep "browser_download_url" | grep linux | cut -d '"' -f 4 | wget -i - && \
     sha256sum -c snyk-linux.sha256 && \
     mv snyk-linux /usr/local/bin/snyk && \
     chmod +x /usr/local/bin/snyk
