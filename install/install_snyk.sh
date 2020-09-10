7eced27196809296c061887b70255c936703806d
Gareth Rushgrove <gareth@morethanseven.net>
2020-09-10 22:42:22 +0100

    Make debugging script easier

--- install/install_snyk.sh
+++ install/install_snyk.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
+
+echo "Installing Snyk (${INPUT_VERSION})"
 
 curl -s https://api.github.com/repos/snyk/snyk/releases/${INPUT_VERSION} | grep "browser_download_url" | grep linux | cut -d '"' -f 4 | wget -i - && \
     sha256sum -c snyk-linux.sha256 && \
